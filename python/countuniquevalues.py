511381cae37540dd6c15932a3a733a50c001ed76
David Veytsman <david.veytsman@gmail.com>
2024-02-08 23:12:03 -0500

    updating count_unique_values function in python

--- python/countuniquevalues.py
+++ python/countuniquevalues.py
@@ -2,5 +2,17 @@
 # the unique values in the array.
 
 def count_unique_values(list):
-    current = 0
-    scout = 0
+    lastSeen = ""
+    count = 0
+    if len(list) == 0:
+        return 0
+    for value in list:
+        if lastSeen != value:
+            count += 1
+            lastSeen = value
+    return count
+
+
+print(count_unique_values([1, 1, 3, 4, 5, 5, 19])); # => 5
+print(count_unique_values([1, 1, 1, 1, 1])); # => 1
+print(count_unique_values([])); # => 0
